307a9784f425417aca7bec7524102923a795a83b
ncaco <ncaco97@gmail.com>
2025-05-03 15:36:06 +0900

    headerp

--- src/main/frontend/src/widgets/layouts/ALayout/ui/components/Header.tsx
+++ src/main/frontend/src/widgets/layouts/ALayout/ui/components/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
     return (
         <>
             {/* 메인 콘텐츠 - 유동적 너비, 전체 높이, 내부 스크롤 */}
-            <header className="flex items-center justify-between px-4 py-2 border-b border-gray-200 bg-gradient-to-r  min-h-[48px]" >
+            <header className="flex md:hidden items-center justify-between px-4 py-2 border-b border-gray-200 bg-gradient-to-r  min-h-[48px]" >
                 {/* 오른쪽: 공간 확보용(정렬용) */}
                 <div className="w-9 h-6" />
 
@@ -22,9 +22,11 @@ export const Header = () => {
                     aria-label="알림"
                 >
                     {/* 간단한 벨 아이콘 (SVG) */}
+                    {/*
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" >
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                     </svg>
+                    */}
                 </button>
             </header>
         </>
--- src/main/frontend/src/widgets/layouts/ALayout/ui/components/LeftComp/topLogo.tsx
+++ src/main/frontend/src/widgets/layouts/ALayout/ui/components/LeftComp/topLogo.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 
 interface TopLogoProps {
     isCollapsed: boolean,
@@ -6,14 +7,12 @@ interface TopLogoProps {
 export const TopLogo = ({ isCollapsed }: TopLogoProps) => {
     return (
         <>
-            <div className="flex-shrink-0 mb-8 flex justify-center">
-                <div className={`flex ${isCollapsed ? 'justify-center' : 'items-center'}`}>
-                    <div className="w-10 h-10 rounded bg-red-600 flex items-center justify-center">
-                        <span className="font-bold text-sm text-white">FSD</span>
-                    </div>
-                    {!isCollapsed && <span className="ml-3 text-xl font-semibold text-gray-900 truncate whitespace-nowrap">아키텍처</span>}
+            <Link to="/a" className={`flex-shrink-0 py-4 flex ${isCollapsed ? 'justify-center' : 'items-center'} justify-center border-b border-gray-200 cursor-pointer group`} tabIndex={0}>
+                <div className="w-10 h-10 rounded bg-red-600 flex items-center justify-center group-hover:bg-red-700 transition-colors">
+                    <span className="font-bold text-sm text-white">FSD</span>
                 </div>
-            </div>
+                {!isCollapsed && <span className="ml-3 text-xl font-semibold text-gray-900 truncate whitespace-nowrap">아키텍처</span>}
+            </Link>
         </>
     )
 }
